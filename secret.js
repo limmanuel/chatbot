2a3da96ff5e58ec2152c9c2a2878f6b345f10306
Limman Lopez <limman@Wizys-MacBook-Air.local>
2018-03-22 16:33:31 +0800

    add url

---
+++ secret.js
@@ -0,0 +1,8 @@
+module.exports.CLIENT = {
+  ID: '1629180653795587',
+  SECRET: '6b736af97f488b85d490cef94d2b3635',
+  URL: 'https://wizyfb-chatbot.herokuapp.com/login/facebook/return'
+};
+module.exports.mongoURL = "mongodb://admin:admin@ds251988.mlab.com:51988/facebook";
+module.exports.qnaMakerSK = "c0801266071945af97de95044add658a";
+
