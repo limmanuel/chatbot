aaac7551624987e7ce2daa8ac57b12d963685630
Limman Lopez <limman@Wizys-MacBook-Air.local>
2018-04-12 16:01:35 +0800

    messenger plugin

--- secret.js
+++ secret.js
@@ -1,7 +1,7 @@
 module.exports.CLIENT = {
   ID: '1629180653795587',
   SECRET: '6b736af97f488b85d490cef94d2b3635',
-  URL: 'https://wizyfb-chatbot.herokuapp.com/login/facebook/return'
+  URL: 'https://f0731367.ngrok.io/login/facebook/return'
 };
 module.exports.mongoURL = "mongodb://admin:admin@ds251988.mlab.com:51988/facebook";
 module.exports.qnaMakerSK = "c0801266071945af97de95044add658a";
